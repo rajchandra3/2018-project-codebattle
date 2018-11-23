ae01267c65a1d3ef46b7bd7016f7715608543a71
alebor-5 <alebor-5@student.ltu.se>
2018-11-23 20:03:57 +0100

    Added a task Schema. The game page now loads a task from the database.

--- controllers/game.js
+++ controllers/game.js
@@ -1,7 +1,16 @@
 
+const Task = require('../models/task');
 
 exports.get = function(req, res){
-    res.render('game/match');
+  Task.findOne({}, function (err, task){
+    if(!task){
+      console.log("error");
+    }
+    else{
+      res.render('game/match',{task_description: task.description});
+    }
+  });
+ 
   };
 
   
