a01368a3612d2721afecd173e87be2199415e41c
alebor-5 <alebor-5@student.ltu.se>
2018-12-01 10:18:03 -0800

    Now the code is extraced from the IDE and sent to the nodejs server inside the docker container and returns the evaluated code

--- code_environment/app.js
+++ code_environment/app.js
@@ -1,20 +1,28 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
 
+
+
+
+
+  
+
 // App
 const app = express();
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
 app.post('/receive', (req, res) => {
-    res.send('Got it mate\n');
-    console.log("received");
+    res.send(eval(req.body.js_code).toString()) 
   });
 
 app.listen(PORT, HOST);
--- controllers/game.js
+++ controllers/game.js
@@ -1,8 +1,8 @@
-
+const request = require('request');
 const Task = require('../models/task');
 
 exports.get = function(req, res){
-  Task.findOne({}, function (err, task){
+  Task.findOne({}, function (err, task){ //This should be randomized
     if(!task){
       console.log("error");
     }
@@ -10,7 +10,19 @@ exports.get = function(req, res){
       res.render('game/match',{task_description: task.description});
     }
   });
- 
   };
+
+  exports.post = function(req,res){
+    const jscode = req.body.code;
+    request({
+        url: "http://0.0.0.0:4000/receive",
+        method: "POST",
+        json: true,   // <--Very important!!!
+        body: {js_code: jscode}
+    }, function (error, response, body){
+        res.render('game/codesubmited',{evaluated_code:body});
+    });
+    
+  }  
 
   
